65c4743a0c1f9cf64aa1d10ae4dd69d50fbe5170
TXT-38 <txt-38@TXT-38.lan1>
2021-07-07 23:08:08 -0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {ViewChild, ElementRef} from '@angular/core';
+
+// make sure google is undefined error isnt there
+declare var google: any;
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  map:any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
   constructor() {}
+
+  // ionic lifestyle method each time page opens the code will run and create a new map
+  ionViewDidEnter(){
+    this.ShowMap();
+  }
+
+  ShowMap() {
+    const location = new google.maps.LatLng(33.946530576355585, -118.30882977911254)
+    const options = {
+      center:location,
+      zoom:15,
+      disableDefaultUI:true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 
 }
